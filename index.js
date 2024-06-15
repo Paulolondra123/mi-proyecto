ce5ba02b2306f07f63d61ade0ce053a1d696cc6b
Paulolondra123 <76613086abc@gmail.com>
2024-06-15 13:14:33 -0400

    aa

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Hola desde el backend!' }); 
 }); 
  
-/* app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-}); */
+});
 
  
 app.listen(PORT, () => { 
